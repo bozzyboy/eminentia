8224017797133f902c7894f4be46fa4b5149c73f
Adriano Viana <adrianoviana87@gmail.com>
2017-05-19 00:27:24 -0300

    Initial commit.

---
+++ src/effects/expello/ExpelloApp.cpp
@@ -0,0 +1,71 @@
+#include "ExpelloApp.h"
+
+//--------------------------------------------------------------
+void ExpelloApp::setup(){
+
+}
+
+//--------------------------------------------------------------
+void ExpelloApp::update(){
+
+}
+
+//--------------------------------------------------------------
+void ExpelloApp::draw(){
+
+}
+
+//--------------------------------------------------------------
+void ExpelloApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ExpelloApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ExpelloApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ExpelloApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ExpelloApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ExpelloApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ExpelloApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ExpelloApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ExpelloApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ExpelloApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ExpelloApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
